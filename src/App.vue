d5f786d53848b783a9693046d44408dfc84b9c7e
NorbertAll <norbert_gutkowski@interia.pl>
2022-12-02 23:43:28 +0100

    project structure

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1>Hel</h1>
+</template>
+
+<style scoped>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat', sans-serif;
+}
+body{
+  background-color: rgba(240, 248, 255, 0.163);
+  color: black;
+}
+</style>
